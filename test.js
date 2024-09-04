457957facf88bf5d776727e3ae06f151e1474f4a
sadiq2001 <80308130+sadiq2001@users.noreply.github.com>
2024-09-04 11:23:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const sum = require('./app');
 const assert = require('assert');
 
 describe('Sum Function', () => {
-  it('should return 5 when adding 2 and 3', () => {
-    assert.strictEqual(sum(2, 3), 5);
+  it('should return 5 when adding 2 and 4', () => {
+    assert.strictEqual(sum(2, 4), 6);
   });
 
   it('should return 0 when adding -1 and 1', () => {
